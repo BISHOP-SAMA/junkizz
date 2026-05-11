43950df1eaec294efe02eb899e7004d4d2f5c261
BISHOP-SAMA <realbishop001@gmail.com>
2026-05-11 19:07:49 +0100

    Delete functions/api directory

--- functions/api/auth/logout.ts
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      'Location': 'https://planetslog.xyz',
-      'Set-Cookie': 'session=; Path=/; HttpOnly; Secure; Max-Age=0',
-    },
-  });
-}
